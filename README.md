978ab98f5a50910bb429227bd2e3caf79369b81f
Maayan Altarac <maayanalt@gmail.com>
2024-12-04 07:21:08 +0200

    refactor on code

--- README.md
+++ README.md
@@ -1,1 +1,34 @@
 # home-assignment
+
+This project demonstrates automated testing for a simple e-commerce website. It covers a core user flow scenario and includes additional bonus tasks for a more comprehensive test suite.
+
+The main objective is to validate user interactions such as navigation, sorting, adding products to the cart, and proceeding to checkout.
+
+## 📌Features
+
+- Automated navigation through the e-commerce site.
+- Verification of product sorting and cart operations.
+- Detailed logging and assertions for test validation.
+- Bonus tasks like selecting a shipping method and completing the checkout process.
+
+## 💻Technologies Used
+
+- Programming Language: TypeScript
+- Automation Tool: Playwright
+
+## 📝Project Structure
+
+ecommerce-automation/
+├── tests/ # Automated test scripts
+│ ├── sanity.spec.ts # All test
+├── pages/ # Page Object Model implementations
+│ ├── AccessoriesPage.ts # Accessories page object
+│ ├── CartPage.ts # Cart page object
+│ ├── CheckoutPage.ts # Checkout page object
+│ ├── ProductDetailsPage.ts # Product Details page object
+├── helpers/ # Helpers files
+│ ├── ApplicationURL.ts # Variables for URLs
+│ ├── utils.ts # Utility functions
+├── types/ # TypeScript types
+│ ├── product.ts # Interface for Product
+├── README.md # Documentation
--- tests/sanity.spec.ts
+++ tests/sanity.spec.ts
@@ -6,38 +6,49 @@ import CartPage from "../pages/CartPage";
 import CheckoutPage from "../pages/CheckoutPage";
 
 test("Validate shopping flow", async ({ page }) => {
+  // Navigate to the application
   await page.goto(ApplicationURL.BASE_URL);
-  await page.getByRole("link", { name: "Accessories", exact: true }).click();
 
+  // Navigate to Accessories Page
+  await page.getByRole("link", { name: "Accessories", exact: true }).click();
   const accessoriesPage = new AccessoriesPage(page);
   // Wait for no more network requests
   await page.waitForLoadState("networkidle");
 
+  // Fetch and verify sorted product list
   const productSortLowToHigh = await accessoriesPage.getAllProducts();
   productSortLowToHigh.sort((a, b) => a.price - b.price);
   await accessoriesPage.verifySorting(productSortLowToHigh);
 
+  // Select the cheapest product and navigate to its details page
   const cheapestProduct = productSortLowToHigh[0];
   await page
     .locator("#main")
     .getByRole("link", { name: `${cheapestProduct.name}` })
     .click();
+
+  // Add product to cart
   const productDetailsPage = new ProductDetailsPage(page);
   const productName = await productDetailsPage.getNameInString();
   const productPrice = await productDetailsPage.getPriceInString();
   await page.getByRole("button", { name: "Add to cart" }).click();
 
+  // Verify product in cart
   await page.getByRole("link", { name: "View cart" }).click();
   const cartPage = new CartPage(page);
   await cartPage.verifyProductInCart(productName, productPrice);
 
+  // Select shipping method and verify subtotal update
   await page.getByLabel("Registered Mail (5 - 8 days)").check();
   const methodLocator = page.locator(
     'label:has-text("Registered Mail (5 - 8 days)")'
   );
   await cartPage.validateSubtotalUpdate(methodLocator);
+
+  // Proceed to checkout
   await page.getByRole("link", { name: "Proceed to checkout" }).click();
 
+  // Fill checkout form
   const checkoutPage = new CheckoutPage(page);
   await checkoutPage.fillMandatoryFields(
     "Maayan",
@@ -49,5 +60,7 @@ test("Validate shopping flow", async ({ page }) => {
     "052-7863423",
     "test@gmail.com"
   );
+
+  // Place order
   await checkoutPage.placeOrder();
 });
